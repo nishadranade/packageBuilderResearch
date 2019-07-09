9e14ef5953c173725b3b45fa01348c876b66e0ed
nishadranade <nranade@umass.edu>
2019-07-08 23:29:34 -0400

    minor additions to distance generator

--- pMetricGenerator.py
+++ pMetricGenerator.py
@@ -13,4 +13,14 @@ def generateScenarios(n):
 
 def generateDistances(n):
     distances = []
+    # initialize all distances to be -1
+    negOne = []
+    for i in range(0, n):
+        negOne.append(-1)
+    for i in range(0, n):
+        distances.append(negOne)
+    # now randomly generate distances between 1 and 99, with all the values along the diagonal set to 100
     return distances
+
+print(generateDistances(5))
+    
