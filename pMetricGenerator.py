2aaba6d7892ecc6d235663fa440d8e1dc465b064
nishadranade <nranade@umass.edu>
2019-07-09 23:11:04 -0400

    distances matrix generator added

--- pMetricGenerator.py
+++ pMetricGenerator.py
@@ -1,7 +1,8 @@
 import numpy as np
+import random
+import copy
 
-# method to generate distances matrix
-
+# method to generate scenarios and their probabilities
 def generateScenarios(n):
     scenarios = []
     for i in range(0, n):
@@ -11,6 +12,7 @@ def generateScenarios(n):
         scenarios[i] = scenarios[i]/total
     return scenarios
 
+# method to generate the distances matrix
 def generateDistances(n):
     distances = []
     # initialize all distances to be -1
@@ -18,9 +20,20 @@ def generateDistances(n):
     for i in range(0, n):
         negOne.append(-1)
     for i in range(0, n):
-        distances.append(negOne)
+        distances.append(copy.deepcopy(negOne))
     # now randomly generate distances between 1 and 99, with all the values along the diagonal set to 100
+    for i in range(0, n):
+        for j in range(0, n):
+            if i == j:
+                distances[i][j] = 100
+            if distances[i][j] == -1 and distances[j][i] == -1:
+                distances[i][j] = 99*random.random()
+                distances[j][i] = distances[i][j]
     return distances
 
-print(generateDistances(5))
-    
+# b = generateDistances(3)
+# for i in b:
+#     print(i)
+
+def generate(n):
+    return generateScenarios(n), generateDistances(n)
