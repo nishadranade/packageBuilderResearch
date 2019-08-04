a3d38ba3a0d9738a5df3bbd612f338558539f6ef
nishadranade <nranade@umass.edu>
2019-08-03 21:23:00 -0400

    minor

--- backwardReduction/pMetricGeneral1.py
+++ backwardReduction/pMetricGeneral1.py
@@ -1,4 +1,10 @@
 # Optimized implementation of Backward Reduction
 # Store the distances in a list of heaps
 # Need to somehow store the indexes from the distances matrix in the same order as the heap, so that removing and changing distances matrix is easy
-# Data structures to use - Priority queue? 
+# Data structures to use - Priority queue? x
+
+# use sorted list - make a list of pairs - (distance, index), and sort based on distances
+# ignore a scenario when storing corresponding sorted list since the indexes are already part of the tuple
+# put results in a file
+
+
