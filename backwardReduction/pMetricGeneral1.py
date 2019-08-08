c9520b9a3c672b19b68c1243bc8a88d4f2954523
nishadranade <nranade@umass.edu>
2019-08-08 19:47:57 -0400

    basic structure for presort

--- backwardReduction/pMetricGeneral1.py
+++ backwardReduction/pMetricGeneral1.py
@@ -7,4 +7,25 @@
 # ignore a scenario when storing corresponding sorted list since the indexes are already part of the tuple
 # put results in a file
 
+from sortedcontainers import SortedList
 
+# method to create the presorted SortedLists based on distances for every scenario 
+def preSort(scenarios, distances):
+    sortedDist = []
+    for i in range(0, len(scenarios)):
+        scenarioDist = SortedList()
+        for j in distances[i]:
+            if i == j:
+                continue
+            scenarioDist.add((distances[i][j], j))
+        sortedDist.append(scenarioDist)
+    return sortedDist
+
+# method to eliminate a single scenario
+def eliminate(scenarios, sortedList):
+
+
+
+# method to redistribute the probabilities and fixing the distances matrix, SortedList after elimination
+def redistribute(scenarios, sortedDist, dsitances):
+    
