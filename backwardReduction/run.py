993edb82551af0570c40b9d22ce683c8aec625ca
nishadranade <nranade@umass.edu>
2019-09-07 14:16:29 -0400

    scripts added

--- backwardReduction/run.py
+++ backwardReduction/run.py
@@ -2,6 +2,7 @@ from generator import *
 #from pMetricGeneral import *
 from pMetricGeneral3 import *
 import sys
+import time
 
 # method to nicely print matrices
 def printMatrix(matrix):
@@ -19,16 +20,20 @@ def printMatrix(matrix):
 # Super script to generate scenarios and distances and then do backward reduction on them
 # Arguements are number of scenarios, and number of to be eliminated for eg python run.py 8 4  
 if __name__=="__main__":
+    start = time.time()
     n = int(sys.argv[1])
     k = int(sys.argv[2])
     data = generate(n)
-    print("Scenarios:")
-    print(data[0])
+    # print("Scenarios:")
+    # print(data[0])
     # print("Distances:")
     # printMatrix(data[1])
-    print()
+    # print()
     result = eliminateK(data[0], data[1], k)
-    print("Reduced Scenarios:")
-    print(result[0])
+    end = time.time()
+    runTime = end - start
+    print(str(n) + ", " + str(k) + ", " + str(runTime))
+    # print("Reduced Scenarios:")
+    # print(result[0])
     # print("Reduced Scenario Distances Matrix")
     # printMatrix(result[1])
