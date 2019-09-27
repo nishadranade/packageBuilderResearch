a73ff374a8d92398dbd01ace5f203862d2292be4
nishadranade <nranade@umass.edu>
2019-09-27 12:48:39 -0400

    comment

--- backwardReduction/generator.py
+++ backwardReduction/generator.py
@@ -14,24 +14,24 @@ def generateScenarios(n):
         # scenarios[i] = scenarios[i]/total
     return scenarios
 
-# method to generate the distances matrix
-def generateDistances(n):
-    distances = []
-    # initialize all distances to be -1
-    negOne = []
-    for i in range(0, n):
-        negOne.append(-1)
-    for i in range(0, n):
-        distances.append(copy.deepcopy(negOne))
-    # now randomly generate distances between 1 and 99, with all the values along the diagonal set to 100
-    for i in range(0, n):
-        for j in range(0, n):
-            if i == j:
-                distances[i][j] = 0         # replace the 0 with a 100 if attempting to run runOld.py
-            if distances[i][j] == -1 and distances[j][i] == -1:
-                distances[i][j] = 99*random.random()
-                distances[j][i] = distances[i][j]
-    return distances
+# method to generate the distances matrix ***OBSOLETE***
+# def generateDistances(n):
+#     distances = []
+#     # initialize all distances to be -1
+#     negOne = []
+#     for i in range(0, n):
+#         negOne.append(-1)
+#     for i in range(0, n):
+#         distances.append(copy.deepcopy(negOne))
+#     # now randomly generate distances between 1 and 99, with all the values along the diagonal set to 100
+#     for i in range(0, n):
+#         for j in range(0, n):
+#             if i == j:
+#                 distances[i][j] = 0         # replace the 0 with a 100 if attempting to run runOld.py
+#             if distances[i][j] == -1 and distances[j][i] == -1:
+#                 distances[i][j] = 99*random.random()
+#                 distances[j][i] = distances[i][j]
+#     return distances
 
 def generateValues(n):
     values = np.random.normal(0, 1, n)
