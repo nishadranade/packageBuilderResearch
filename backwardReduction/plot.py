177164471ff2a47c042ae94a73026cd41b04603c
Kyaw Htet <harry@1x-nat-vl931-172-30-129-53.wireless.umass.edu>
2019-09-27 13:08:17 -0400

    plot.py updated

--- backwardReduction/plot.py
+++ backwardReduction/plot.py
@@ -9,40 +9,56 @@ with open(input) as file:
 
 data = data.split('\n')
 
-x = [int(row.split(', ')[0]) for row in data]
+numScenario = [float(row.split(', ')[0]) for row in data]
 
-y = [int(row.split(', ')[2]) for row in data]
+numScenarioEliminated = [float(row.split(', ')[1]) for row in data]
 
-#dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+time = [float(row.split(', ')[2]) for row in data]
 
-#dev_y = [38496, 42000, 46752, 49320, 53200,
-#         56000, 62316, 64928, 67317, 68748, 73752]
 
-x_max = max(x)
-x_min = min(x)
-x_interval = x_max//len(x)
+numScenario_max = max(numScenario)
+numScenario_min = min(numScenario)
+numScenario_interval = numScenario_max//len(numScenario)
 
-y_max = max(y)
-y_min = min(y)
-y_interval = y_max//len(y)
+numScenarioEliminated_max = max(numScenarioEliminated)
+numScenarioEliminated_min = min(numScenarioEliminated)
+numScenarioEliminated_interval = numScenarioEliminated_max//len(numScenarioEliminated)
 
-plt.scatter(x, y)
+time_max = max(time)
+time_min = min(time)
+time_interval = time_max//len(time)
 
-plt.plot(x, y)
+# Number of Scenarios vs. Time
+plt.subplot(2, 2, 1)
+plt.scatter(numScenario, time)
+plt.plot(numScenario, time)
 
 plt.title('Number of Scenarios vs. Time')
-
 plt.xlabel('Number of Scenarios')
-
 plt.ylabel('Time (s)')
+plt.xticks(np.arange(0, 1.5*numScenario_max, numScenario_interval)) 
+plt.xticks(rotation=45, ha="right")
+plt.yticks(np.arange(0, 1.5*time_max, time_interval))
+#plt.yticks(rotation=45, ha="right")
+#plt.axis([numScenario_min, numScenario_max, time_min, time_max])
+plt.grid()
+#plt.show()
 
-#plt.xticks(np.arange(0, x_max, x_interval)) 
 
-#plt.yticks(np.arange(0, y_max, y_interval))
-
+# Number of Scenarios Eliminated vs. Time
+plt.subplot(2, 2, 2)
+plt.scatter(numScenarioEliminated, time)
+plt.plot(numScenarioEliminated, time)
+plt.title('Number of Scenarios Eliminated vs. Time')
+plt.xlabel('Number of Scenarios Eliminated')
+plt.ylabel('Time (s)')
+plt.xticks(np.arange(0, 1.5*numScenarioEliminated_max, numScenarioEliminated_interval))
+plt.xticks(rotation=45, ha="right")
+plt.yticks(np.arange(0, 1.5*time_max, time_interval))
+#plt.yticks(rotation=45, ha="right")
+#plt.axis([numScenarioEliminated_min, numScenarioEliminated_max, time_min, time_max])
 plt.grid()
 
-plt.axis([x_min, x_max, y_min, y_max])
-
 plt.show()
+
 
