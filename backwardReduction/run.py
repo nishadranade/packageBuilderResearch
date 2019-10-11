33ff9e2be18119edeee412111bfa5b2866a5947d
harryhtet <harryhtet@gmail.com>
2019-10-10 23:54:34 -0400

    savePlot added

--- backwardReduction/run.py
+++ backwardReduction/run.py
@@ -1,7 +1,8 @@
 from generator import *
 from pMetricGeneral3 import *
-from scatter import scatterPlot
+from scatter import *
 from matplotlib import pyplot as plt
+import copy
 import sys
 import time
 
@@ -9,6 +10,20 @@ import time
 def printMatrix(matrix):
     for i in matrix:
         print(i)
+
+def savePlot(data, n, k):
+    data_1 = data
+    temp = copy.deepcopy(data_1)
+
+    ratio = k / n
+    interval = int((ratio / 3)*n)
+    for i in range(0, k+1, interval):
+        result = eliminateK(data_1[0], data_1[1][1], i, data_1[1][0])    #fix
+        reduced_values = result[2]
+        reduced_probabilities = result[0]
+        singleScatterPlot(reduced_values, reduced_probabilities, n, i)
+        data_1 = copy.deepcopy(temp)
+
 
 # Super script to generate scenarios and distances and then do backward reduction on them
 # Arguements are number of scenarios, and number of to be eliminated for eg python run.py 8 4  
@@ -22,8 +37,8 @@ if __name__=="__main__":
     # print("Distances:")
     # printMatrix(data[1])
     # print()
-    #scatterPlot(data[1][0])
     original = data[1][0]
+    original_probabilities = copy.deepcopy(data[0])
     result = eliminateK(data[0], data[1][1], k, data[1][0])
     end = time.time()
     runTime = end - start
@@ -32,6 +47,12 @@ if __name__=="__main__":
     #print(result[0])
     # print("Reduced Scenario Distances Matrix")
     # printMatrix(result[1])
-    #scatterPlot(result[2])
+
+    data_1 = generate(n, 0, 1, 5, 2)
+    savePlot(data_1, n, k)
+
     reduced = result[2]
-    scatterPlot(original, reduced)
+    reduced_probabilities = result[0]
+    #scatterPlot(original, original_probabilities, reduced, reduced_probabilities)
+    #singleScatterPlot(reduced, reduced_probabilities, n, k)
+    
